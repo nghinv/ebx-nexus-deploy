1278f4649ac73471a3cadf2e1f2c27bfd267ea86
Mickael GERMEMONT <mickaelgermemont@gmail.com>
2019-03-07 12:52:09 -0500

    updated pslib custom dependencies script

--- pushPsLibs.sh
+++ pushPsLibs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mvn --settings settings.xml deploy:deploy-file -Dfile="$libFolder"/presales-toolbox.jar	-DartifactId=presales.toolbox -Dpackaging=jar -Dversion="$pslibVersion"  -DgroupId="$groupIdPsLibs" -DrepositoryId="$repoId" -Durl="$repoUrl"
+mvn --settings settings.xml deploy:deploy-file -Dfile="$libFolder"/presales-toolbox.jar	        -DartifactId=presales.toolbox -Dpackaging=jar -Dversion="$pslibVersion"  -DgroupId="$groupIdPsLibs" -DrepositoryId="$repoId" -Durl="$repoUrl"
+mvn --settings settings.xml deploy:deploy-file -Dfile="$libFolder"/adminpermissionsutil-1.0.jar	-DartifactId=adminpermissionsutil -Dpackaging=jar -Dversion="$pslibVersion"  -DgroupId="$groupIdPsLibs" -DrepositoryId="$repoId" -Durl="$repoUrl"
